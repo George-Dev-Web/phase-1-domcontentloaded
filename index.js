a8d29baea2f4f3817e75685d8c574679d2d80130
George <georgemwazuna3@gmail.com>
2025-03-22 13:08:51 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+  let element = document.getElementById("text"); // Ensure this ID exists in your HTML
+  element.textContent = "This is really cool!";
+});
